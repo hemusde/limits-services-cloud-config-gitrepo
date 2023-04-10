46ec55daeb9df042fd9c39e85ebca8f66b756f0d
hemusde <76616058+hemusde@users.noreply.github.com>
2023-04-10 17:00:10 +0530

    Add files via upload

---
+++ limits-service/src/test/java/com/hemu/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemu.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
